b5c527d7e68660360f6c0a5216d771e49c28a638
Caio Balduino <caiobrafachino@hotmail.com>
2025-03-19 15:35:52 -0300

    💫feat: prevents typing of non-numeric characters

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const amount = document.getElementById("amount");
+
+amount.addEventListener("input", () => {
+  // Não permitimos a digitação de textos no input
+  const hasCharactersRegex = /\D+/g;
+  // \D → Representa qualquer caractere que não seja um dígito numérico
+  // + → Indica que deve haver pelo menos um ou mais caracteres que não sejam números.
+  // g (global) → Faz com que a busca ocorra em toda a string, e não apenas na primeira ocorrência.
+  amount.value = amount.value.replace(hasCharactersRegex, ""); // troca qualquer letra por 'nada' -> ""
+});
